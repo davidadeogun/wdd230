f4591b51e47877e59f154eec04fb982444a6dc30
Your Name <you@example.com>
2023-01-28 19:07:14 -0500

    week4_homepage

---
+++ chamber/scripts/JS.js
@@ -0,0 +1,23 @@
+function toggleMenu() {
+    document.querySelector("#primaryNav").classList.toggle("open");
+    document.querySelector("#hamburgerBtn").classList.toggle("open");
+
+}
+
+const x = document.querySelector("#hamburgerBtn");
+x.onclick = toggleMenu;
+
+let oLastModif = document.lastModified;
+let el = document.querySelector("#currently");
+el.innerHTML = "Last Modification: "+ oLastModif;
+
+
+const datefield = document.querySelector(".currentDate");
+
+const now = new Date();
+
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+	dateStyle: "full"
+}).format(now);
+
+datefield.innerHTML = `${fulldateUK}`;
