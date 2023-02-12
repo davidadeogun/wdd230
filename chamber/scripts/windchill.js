5a4a39e03d792a77ac83a8f7bb41814111aa4be1
Your Name <you@example.com>
2023-02-11 23:45:24 -0500

    .

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,18 +1,25 @@
-let windSpeedKm = document.querySelector("#wind-speed-value").value;
-let tempCelsius = document.querySelector("#temp-value").value;
+let windSpeedKm = parseFloat(document.querySelector("#wind-speed-value").innerHTML);
+let tempCelsius = parseInt(document.querySelector("#temp-value").innerHTML);
 
+//Convert Celsius to fahrenheit
 let temFah = (tempCelsius * 9 / 5) + 32;
+
+//Convert Km to Miles
 let windSpeedMiles = windSpeedKm * 1.609;
 
+
+//Calculate windchill
 let windChill = 35.74 + 0.6215 * temFah - 35.75 * Math.pow(windSpeedMiles, 0.16) + 0.4275 * temFah * Math.pow(windSpeedMiles, 0.16);
 
+//store function as a variable
 let displayResult = function() {
   if (windSpeedMiles > 3 && temFah <= 50) {
-    document.querySelector("#wind-chill-display").textContent = windChill;
+    document.querySelector("#wind-chill-display").textContent = windChill.toFixed(2) + "°F";
   } else {
     document.querySelector("#wind-chill-display").textContent = "N/A";
   }
 };
 
+//call the function 
 displayResult();
 
