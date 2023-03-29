e541375df65b1898e4a59899c2c6658f2567c8c8
Your Name <you@example.com>
2023-03-29 14:30:36 -0400

    .

--- Bountiful/script/fruitmix.js
+++ Bountiful/script/fruitmix.js
@@ -1,65 +1,65 @@
 function populateSelect(select, fruits) {
-    fruits.forEach(fruit => {
-      const option = document.createElement("option");
-      option.value = fruit.id;
-      option.textContent = fruit.name;
-      select.appendChild(option);
-    });
+  fruits.forEach(fruit => {
+    const option = document.createElement("option");
+    option.value = fruit.id;
+    option.textContent = fruit.name;
+    select.appendChild(option);
+  });
+}
+
+function displayOutput(order) {
+  const outputElement = document.getElementById("output");
+  const orderDate = new Date().toLocaleString();
+
+  const output = `
+Order Details:
+First Name: ${order.firstName}
+Email: ${order.email}
+Phone: ${order.phone}
+Fruit 1: ${order.fruit1.name}
+Fruit 2: ${order.fruit2.name}
+Fruit 3: ${order.fruit3.name}
+Special Instructions: ${order.specialInstructions}
+Order Date: ${orderDate}
+
+Total Nutrients:
+Carbohydrates: ${(order.fruit1.nutritions.carbohydrates + order.fruit2.nutritions.carbohydrates + order.fruit3.nutritions.carbohydrates).toFixed(2)} g
+Protein: ${(order.fruit1.nutritions.protein + order.fruit2.nutritions.protein + order.fruit3.nutritions.protein).toFixed(2)} g
+Fat: ${(order.fruit1.nutritions.fat + order.fruit2.nutritions.fat + order.fruit3.nutritions.fat).toFixed(2)} g
+Sugar: ${(order.fruit1.nutritions.sugar + order.fruit2.nutritions.sugar + order.fruit3.nutritions.sugar).toFixed(2)} g
+Calories: ${(order.fruit1.nutritions.calories + order.fruit2.nutritions.calories + order.fruit3.nutritions.calories).toFixed(2)} kcal
+  `;
+
+  outputElement.textContent = output;
+}
+
+
+function updateSubmittedDrinksCount() {
+  let count = localStorage.getItem("submittedDrinksCount");
+  count = count ? parseInt(count) + 1 : 1;
+  localStorage.setItem("submittedDrinksCount", count);
+  displaySubmittedDrinksCount();
+}
+
+function displaySubmittedDrinksCount() {
+  const count = localStorage.getItem("submittedDrinksCount") || 0;
+  const infoCard = document.getElementById("info-card");
+  if (infoCard) {
+    infoCard.textContent = `Total Submitted Specialty Drinks: ${count}`;
   }
-  
-  function displayOutput(order) {
-    const outputElement = document.getElementById("output");
-    const orderDate = new Date().toLocaleString();
-  
-    const output = `
-  Order Details:
-  First Name: ${order.firstName}
-  Email: ${order.email}
-  Phone: ${order.phone}
-  Fruit 1: ${order.fruit1.name}
-  Fruit 2: ${order.fruit2.name}
-  Fruit 3: ${order.fruit3.name}
-  Special Instructions: ${order.specialInstructions}
-  Order Date: ${orderDate}
-  
-  Total Nutrients:
-  Carbohydrates: ${(order.fruit1.nutritions.carbohydrates + order.fruit2.nutritions.carbohydrates + order.fruit3.nutritions.carbohydrates).toFixed(2)} g
-  Protein: ${(order.fruit1.nutritions.protein + order.fruit2.nutritions.protein + order.fruit3.nutritions.protein).toFixed(2)} g
-  Fat: ${(order.fruit1.nutritions.fat + order.fruit2.nutritions.fat + order.fruit3.nutritions.fat).toFixed(2)} g
-  Sugar: ${(order.fruit1.nutritions.sugar + order.fruit2.nutritions.sugar + order.fruit3.nutritions.sugar).toFixed(2)} g
-  Calories: ${(order.fruit1.nutritions.calories + order.fruit2.nutritions.calories + order.fruit3.nutritions.calories).toFixed(2)} kcal
-    `;
-  
-    outputElement.textContent = output;
-  }
-  
-
-  function updateSubmittedDrinksCount() {
-    let count = localStorage.getItem("submittedDrinksCount");
-    count = count ? parseInt(count) : 1;
-    count += 1;
-    localStorage.setItem("submittedDrinksCount", count);
-  }
-  
-  function displaySubmittedDrinksCount() {
-    const count = localStorage.getItem("submittedDrinksCount") || 0;
-    const infoCard = document.getElementById("info-card");
-    if (infoCard) {
-      infoCard.textContent = `Total Submitted Specialty Drinks: ${count}`;
-    }
-  }
-  
+}
 
 
 
 
-  function processForm(event) {
-    event.preventDefault();
-  
-    const firstName = document.getElementById("first-name").value;
-    const email = document.getElementById("email").value;
-    const phone = document.getElementById("phone").value;
-    const fruit1Id = document.getElementById("fruits1").value;
+
+function processForm(event) {
+  event.preventDefault();
+
+  const firstName = document.getElementById("first-name").value;
+  const email = document.getElementById("email").value;
+  const phone = document.getElementById("phone").value;
+  const fruit1Id = document.getElementById("fruits1").value;
 const fruit2Id = document.getElementById("fruits2").value;
 const fruit3Id = document.getElementById("fruits3").value;
 const specialInstructions = document.getElementById("special-instructions").value;
@@ -79,8 +79,8 @@ specialInstructions,
 };
 
 displayOutput(order);
-
-displaySubmittedDrinksCount();
+updateSubmittedDrinksCount();
+//displaySubmittedDrinksCount();
 }
 
 let fruits = [];
@@ -95,6 +95,5 @@ populateSelect(document.getElementById("fruits3"), fruits);
 });
 
 document.getElementById("order-form").addEventListener("submit", processForm);
-
 
 
--- Bountiful/script/weather.js
+++ Bountiful/script/weather.js
@@ -1,3 +1,52 @@
+//Lazy loading
+const images = document.querySelectorAll("[data-src]");
+
+
+function preloadImage(img) {
+    const src= img.getAttribute("data-src");
+    if (!src) {
+        return;
+    }
+
+    img.src = src;
+}
+const imgOptions = {
+    threshold: 1,
+    rootMargin: "0px 0px 300px 0px"
+};
+
+const imgObserver = new IntersectionObserver((entries,
+     imgObserver) => {
+       entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            preloadImage(entry.target);
+            imgObserver.unobserve(entry.target);
+        }
+       })
+}, imgOptions);
+
+images.forEach(image => {
+    imgObserver.observe(image);
+});
+
+//End - Lazy Loading
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function toggleMenu() {
   const primaryNav = document.querySelector("#primary-navigation");
   const hamburgerButton = document.querySelector("#hamburgerButton");
@@ -78,7 +127,3 @@ apiFetch(); // Call the function to fetch and display the weather data
 
 
 
-//Animate header text script
-$(document).ready(function() {
-  $('#header-text').fadeIn(5000);
-});
