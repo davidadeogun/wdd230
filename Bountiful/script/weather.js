2bfed7ff9f95cc7e662070203db14f5833d9cd08
Your Name <you@example.com>
2023-03-30 17:10:33 -0400

    ,

--- Bountiful/script/weather.js
+++ Bountiful/script/weather.js
@@ -81,6 +81,8 @@ if (datefield) {
 // End of Last Modified Date script
 
 
+
+
 // WEATHER FORECAST SCRIPT
 const url = "https://api.openweathermap.org/data/3.0/onecall?lat=33.158&lon=-117.351&exclude=minutely,hourly,alerts&appid=c2aca1c7991c8e388fe1ab63e0fe2cc9";
 let WeatherIcon = document.querySelector('#weathericon');
@@ -91,57 +93,74 @@ let FirstDayTemp = document.querySelector('#firstdaytemp');
 let SecondDayTemp = document.querySelector('#seconddaytemp');
 let ThirdDayTemp = document.querySelector('#thirddaytemp');
 
+function getDayName(date) {
+  const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  return days[date.getDay()];
+}
+
+function getNextNDays(n) {
+  const days = [];
+  const today = new Date();
+  for (let i = 0; i < n; i++) {
+    const nextDay = new Date(today);
+    nextDay.setDate(today.getDate() + i);
+    days.push(getDayName(nextDay));
+  }
+  return days;
+}
 
 async function apiFetch() {
   try {
-      const response = await fetch(url);
-      if (response.ok) {
-          const data = await response.json();
-          console.log(data)
-          displayResult(data);
-      } else {
-          throw Error(await response.text());
-      }
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+      displayResult(data);
+    } else {
+      throw Error(await response.text());
+    }
   } catch (error) {
-      console.log(error);
+    console.log(error);
   }
 }
-
 
 apiFetch();
 
 function displayResult(data) {
+  const nextThreeDays = getNextNDays(4);
+
   if (WeatherHumidity) {
-      WeatherHumidity.textContent = data.current.humidity;
+    WeatherHumidity.textContent = data.current.humidity;
   }
 
   if (CurrentTemp) {
-      CurrentTemp.textContent = data.current.temp;
+    CurrentTemp.textContent = `${nextThreeDays[0]}: ${data.current.temp}°F`;
   }
 
   if (FirstDayTemp) {
-    FirstDayTemp.textContent = data.daily[0].temp.day;
+    const temp = data.daily[1].temp.day.toFixed(2);
+   FirstDayTemp.textContent = `${nextThreeDays[1]}: ${temp}°F`;
   }
 
   if (SecondDayTemp) {
-    SecondDayTemp.textContent = data.daily[1].temp.day;
+    const temp = data.daily[2].temp.day.toFixed(2);
+    SecondDayTemp.textContent = `${nextThreeDays[2]}: ${temp}°F`;
   }
 
   if (ThirdDayTemp) {
-    ThirdDayTemp.textContent = data.daily[2].temp.day;
+   const temp = data.daily[3].temp.day.toFixed(2);
+   ThirdDayTemp.textContent = `${nextThreeDays[3]}: ${temp}°F`;
   }
-
-  
-
   if (WeatherIcon && WeatherDescription) {
-      const srcicon = `http://openweathermap.org/img/w/${data.current.weather[0].icon}.png`;
-      const description = data.current.weather[0].description;
+    const srcicon = `http://openweathermap.org/img/w/${data.current.weather[0].icon}.png`;
+    const description = data.current.weather[0].description;
 
-      WeatherIcon.setAttribute('src', srcicon);
-      WeatherIcon.setAttribute('alt', description);
-      WeatherDescription.textContent = description;
+    WeatherIcon.setAttribute('src', srcicon);
+    WeatherIcon.setAttribute('alt', description);
+    WeatherDescription.textContent = description;
   }
 }
+
 
 
 
